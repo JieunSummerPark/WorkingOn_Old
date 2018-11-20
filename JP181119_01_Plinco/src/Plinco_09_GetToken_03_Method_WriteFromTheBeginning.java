c9c6d436582de1cf7078823e57dfc10334845281
JieunSummerPark <parkj2@student.swosu.edu>
2018-11-19 19:54:54 -0600

    Fianally Succeed!!!!!!!!!!! (but still more things to do)

---
+++ JP181119_01_Plinco/src/Plinco_09_GetToken_03_Method_WriteFromTheBeginning.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Plinco_09_GetToken_03_Method_WriteFromTheBeginning {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		input.close();
+		
+
+	}
+
+}
