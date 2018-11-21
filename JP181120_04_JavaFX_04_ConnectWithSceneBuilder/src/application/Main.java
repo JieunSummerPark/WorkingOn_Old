b8bf49427dcd6bbc57d8ca7d64b61ac995c4e8cc
JieunSummerPark <parkj2@student.swosu.edu>
2018-11-20 23:38:32 -0600

    Done!

--- JP181120_04_JavaFX_04_ConnectWithSceneBuilder/src/application/Main.java
+++ JP181120_04_JavaFX_04_ConnectWithSceneBuilder/src/application/Main.java
@@ -1,23 +1,24 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Root.fxml"));
+		Scene scene = new Scene(root);
+		
+		primaryStage.setTitle("Hello JavaFX");
+		primaryStage.setResizable(false);	// 크기 조절 설정 막기
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 	
 	public static void main(String[] args) {
