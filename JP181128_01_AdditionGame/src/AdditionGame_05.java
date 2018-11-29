5a18cb5a3b5bb404cffff9a49a7e9738d2566b2a
JieunSummerPark <parkj2@student.swosu.edu>
2018-11-28 18:14:41 -0600

    Ready for GUI

--- JP181128_01_AdditionGame/src/AdditionGame_05.java
+++ JP181128_01_AdditionGame/src/AdditionGame_05.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class AdditionGame_05 {
 
+	public static Scanner input = new Scanner(System.in);
+
 	public static String userName;
 
 	public static int roundCounter;
-	public static int howManyGames = 3;
+	public static int howManyGames = 7;
 
 	public static int ranNum1;
 	public static int ranNum2;
@@ -27,15 +29,100 @@ public class AdditionGame_05 {
 	public static char keepGoingCheck;
 	public static boolean keepGoing = true;
 
+	public static int whichCalculation;
+
+
+
 
 
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
+
 
 		System.out.print("Please enter your name: ");
 		userName = input.nextLine();
 		System.out.println("You are " + userName + ".");
+
+		System.out.print(""
+				+ "\n1. Addition"
+				+ "\n2. Subtraction"
+				+ "\n3. Multiplication"
+				+ "\n4. Division"
+				+ "\n\nWhich calculation do you want to do? : ");
+
+
+		System.out.println("Which calculation do you want to do?");
+
+		whichCalculation = input.nextInt();
+
+		switch(whichCalculation) {
+		case 1: Addition(); break;
+		case 2: Subtraction(); break;
+		case 3: Multiplication(); break;
+		case 4: Division(); break;
+
+
+		}
+
+
+
+		/**
+		System.out.println("");
+		System.out.println("Let's start Addition Game.");
+		System.out.println("");
+
+		while(keepGoing) {
+
+			for (roundCounter = 1; roundCounter <= howManyGames; roundCounter++) {
+				startTime = System.currentTimeMillis();
+				ranNum1 = (int)(Math.random() * 10 + 1 );
+				ranNum2 = (int)(Math.random() * 10 + 1 );
+				result = ranNum1 + ranNum2;
+				System.out.print(ranNum1 + " + " + ranNum2 + " = ");
+				answer = input.nextInt();
+				if (answer == result) {
+					System.out.println("Correct!");
+					correctCounter++;
+				} else if (answer != result) {
+					System.out.println("It's wrong.\nThe correct answer is " + result + "!");
+				}
+				System.out.println("");
+				stopTime = System.currentTimeMillis();
+				answerTime = (stopTime - startTime) / 1000;
+				totalTime += answerTime;
+			}
+
+			scorePercentage = (correctCounter/(double)howManyGames) * 100;
+			System.out.println(userName + "'s score is " + correctCounter + " out of " + howManyGames + ".");
+			System.out.println(userName + "'s score is " + String.format("%.2f", scorePercentage) + "%.");
+			System.out.println(userName + " took " + String.format("%.2f", totalTime) + " seconds.");
+			System.out.println(userName + "'s average seconds per answer is " + String.format("%.2f", totalTime/howManyGames) + " seconds.");
+
+			correctCounter = 0;
+			totalTime = 0;
+
+			System.out.println("\n");
+			System.out.println("Would you like to try again?");
+			System.out.print("Press any key to try again OR Press '0' to stop: ");
+			keepGoingCheck = input.next().charAt(0);
+			System.out.println("");
+			if(keepGoingCheck == '0') {
+				System.out.println("\nThank you and good bye.");
+				keepGoing = false;
+				break;
+			}
+		}
+		 */
+
+		input.close();
+
+	} // End of Main Method
+
+
+
+
+
+	private static void Addition() {
 
 		System.out.println("");
 		System.out.println("Let's start Addition Game.");
@@ -61,6 +148,7 @@ public class AdditionGame_05 {
 				answerTime = (stopTime - startTime) / 1000;
 				totalTime += answerTime;
 			}
+
 			scorePercentage = (correctCounter/(double)howManyGames) * 100;
 			System.out.println(userName + "'s score is " + correctCounter + " out of " + howManyGames + ".");
 			System.out.println(userName + "'s score is " + String.format("%.2f", scorePercentage) + "%.");
@@ -69,7 +157,7 @@ public class AdditionGame_05 {
 
 			correctCounter = 0;
 			totalTime = 0;
-			
+
 			System.out.println("\n");
 			System.out.println("Would you like to try again?");
 			System.out.print("Press any key to try again OR Press '0' to stop: ");
@@ -82,8 +170,187 @@ public class AdditionGame_05 {
 			}
 		}
 
-		input.close();
+	}
 
-	} // End of Main Method
+
+
+
+
+	private static void Subtraction() {
+
+		System.out.println("");
+		System.out.println("Let's start Subtraction Game.");
+		System.out.println("");
+
+		while(keepGoing) {
+
+			for (roundCounter = 1; roundCounter <= howManyGames; roundCounter++) {
+				startTime = System.currentTimeMillis();
+				ranNum1 = (int)(Math.random() * 10 + 1 );
+				ranNum2 = (int)(Math.random() * 10 + 1 );
+				
+				int max;
+				
+				if (ranNum2 > ranNum1) {
+					max = ranNum2;
+					ranNum2 = ranNum1;
+					ranNum1 = max;
+				}
+				
+				result = ranNum1 - ranNum2;
+				System.out.print(ranNum1 + " - " + ranNum2 + " = ");
+				answer = input.nextInt();
+				if (answer == result) {
+					System.out.println("Correct!");
+					correctCounter++;
+				} else if (answer != result) {
+					System.out.println("It's wrong.\nThe correct answer is " + result + "!");
+				}
+				System.out.println("");
+				stopTime = System.currentTimeMillis();
+				answerTime = (stopTime - startTime) / 1000;
+				totalTime += answerTime;
+			}
+
+			scorePercentage = (correctCounter/(double)howManyGames) * 100;
+			System.out.println(userName + "'s score is " + correctCounter + " out of " + howManyGames + ".");
+			System.out.println(userName + "'s score is " + String.format("%.2f", scorePercentage) + "%.");
+			System.out.println(userName + " took " + String.format("%.2f", totalTime) + " seconds.");
+			System.out.println(userName + "'s average seconds per answer is " + String.format("%.2f", totalTime/howManyGames) + " seconds.");
+
+			correctCounter = 0;
+			totalTime = 0;
+
+			System.out.println("\n");
+			System.out.println("Would you like to try again?");
+			System.out.print("Press any key to try again OR Press '0' to stop: ");
+			keepGoingCheck = input.next().charAt(0);
+			System.out.println("");
+			if(keepGoingCheck == '0') {
+				System.out.println("\nThank you and good bye.");
+				keepGoing = false;
+				break;
+			}
+		}
+
+	}
+
+
+
+
+
+	private static void Multiplication() {
+		System.out.println("");
+		System.out.println("Let's start Addition Game.");
+		System.out.println("");
+
+		while(keepGoing) {
+
+			for (roundCounter = 1; roundCounter <= howManyGames; roundCounter++) {
+				startTime = System.currentTimeMillis();
+				ranNum1 = (int)(Math.random() * 10 + 1 );
+				ranNum2 = (int)(Math.random() * 10 + 1 );
+				result = ranNum1 * ranNum2;
+				System.out.print(ranNum1 + " X " + ranNum2 + " = ");
+				answer = input.nextInt();
+				if (answer == result) {
+					System.out.println("Correct!");
+					correctCounter++;
+				} else if (answer != result) {
+					System.out.println("It's wrong.\nThe correct answer is " + result + "!");
+				}
+				System.out.println("");
+				stopTime = System.currentTimeMillis();
+				answerTime = (stopTime - startTime) / 1000;
+				totalTime += answerTime;
+			}
+
+			scorePercentage = (correctCounter/(double)howManyGames) * 100;
+			System.out.println(userName + "'s score is " + correctCounter + " out of " + howManyGames + ".");
+			System.out.println(userName + "'s score is " + String.format("%.2f", scorePercentage) + "%.");
+			System.out.println(userName + " took " + String.format("%.2f", totalTime) + " seconds.");
+			System.out.println(userName + "'s average seconds per answer is " + String.format("%.2f", totalTime/howManyGames) + " seconds.");
+
+			correctCounter = 0;
+			totalTime = 0;
+
+			System.out.println("\n");
+			System.out.println("Would you like to try again?");
+			System.out.print("Press any key to try again OR Press '0' to stop: ");
+			keepGoingCheck = input.next().charAt(0);
+			System.out.println("");
+			if(keepGoingCheck == '0') {
+				System.out.println("\nThank you and good bye.");
+				keepGoing = false;
+				break;
+			}
+		}
+
+	}
+
+
+
+
+
+	private static void Division() {
+		
+		System.out.println("");
+		System.out.println("Let's start Division Game.");
+		System.out.println("");
+
+		while(keepGoing) {
+
+			for (roundCounter = 1; roundCounter <= howManyGames; roundCounter++) {
+				startTime = System.currentTimeMillis();
+				ranNum1 = (int)(Math.random() * 10 + 1 );
+				ranNum2 = (int)(Math.random() * 10 + 1 );
+				
+				int max;
+				
+				if (ranNum2 > ranNum1) {
+					max = ranNum2;
+					ranNum2 = ranNum1;
+					ranNum1 = max;
+				}
+				
+				result = ranNum1 / ranNum2;
+				System.out.print(ranNum1 + " / " + ranNum2 + " = ");
+				answer = input.nextInt();
+				if (answer == result) {
+					System.out.println("Correct!");
+					correctCounter++;
+				} else if (answer != result) {
+					System.out.println("It's wrong.\nThe correct answer is " + result + "!");
+				}
+				System.out.println("");
+				stopTime = System.currentTimeMillis();
+				answerTime = (stopTime - startTime) / 1000;
+				totalTime += answerTime;
+			}
+
+			scorePercentage = (correctCounter/(double)howManyGames) * 100;
+			System.out.println(userName + "'s score is " + correctCounter + " out of " + howManyGames + ".");
+			System.out.println(userName + "'s score is " + String.format("%.2f", scorePercentage) + "%.");
+			System.out.println(userName + " took " + String.format("%.2f", totalTime) + " seconds.");
+			System.out.println(userName + "'s average seconds per answer is " + String.format("%.2f", totalTime/howManyGames) + " seconds.");
+
+			correctCounter = 0;
+			totalTime = 0;
+
+			System.out.println("\n");
+			System.out.println("Would you like to try again?");
+			System.out.print("Press any key to try again OR Press '0' to stop: ");
+			keepGoingCheck = input.next().charAt(0);
+			System.out.println("");
+			if(keepGoingCheck == '0') {
+				System.out.println("\nThank you and good bye.");
+				keepGoing = false;
+				break;
+			}
+		}
+		
+	}
+
+
 
 }
--- JP181128_01_AdditionGame/src/AdditionGame_06.java
+++ JP181128_01_AdditionGame/src/AdditionGame_06.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
-public class AdditionGame_04 {
+public class AdditionGame_06 {
+
+	public static Scanner input = new Scanner(System.in);
 
 	public static String userName;
 
 	public static int roundCounter;
-	public static int howManyGames = 3;
+	public static int howManyGames = 7;
 
 	public static int ranNum1;
 	public static int ranNum2;
@@ -20,35 +22,64 @@ public class AdditionGame_04 {
 	public static double startTime;
 	public static double stopTime;
 	public static double answerTime;
-	public static double totalTime;
+	public static double totalTime = 0;
 
 	public static char startORstop;
 
 	public static char keepGoingCheck;
 	public static boolean keepGoing = true;
 
+	public static int whichCalculation;
+
+
+
 
 
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
-
 		System.out.print("Please enter your name: ");
 		userName = input.nextLine();
 		System.out.println("You are " + userName + ".");
-
-		System.out.println("");
-		System.out.println("Let's start Addition Game.");
-		System.out.println("");
+		
 
 		while(keepGoing) {
+			
+			System.out.print("\n"
+					+ "\n1. Addition"
+					+ "\n2. Subtraction"
+					+ "\n3. Multiplication"
+					+ "\n4. Division"
+					+ "\n\nWhich calculation do you want to do? : ");
+			whichCalculation = input.nextInt();
+			// TODO 1,2,3,4 말고 다른 거 나왔을 떄 에러표시 만들기
+			
+			if (whichCalculation!=1 && whichCalculation!=2 && whichCalculation!=3 && whichCalculation!=4) {
+				System.out.println("Try 1, 2, 3, or 4.");
+				break;
+			}
+			
+			System.out.println("\n");
+			switch(whichCalculation) {
+			case 1: System.out.println("Let's start Addition Game."); break;
+			case 2: System.out.println("Let's start Subtraction Game."); break;
+			case 3: System.out.println("Let's start Multiplication Game."); break;
+			case 4: System.out.println("Let's start Division Game."); break;
+			}
 
 			for (roundCounter = 1; roundCounter <= howManyGames; roundCounter++) {
 				startTime = System.currentTimeMillis();
+				
+				// TODO 나중에 나눗셈은 ranNum1 더 큰걸로 정해주기
 				ranNum1 = (int)(Math.random() * 10 + 1 );
 				ranNum2 = (int)(Math.random() * 10 + 1 );
-				result = ranNum1 + ranNum2;
-				System.out.print(ranNum1 + " + " + ranNum2 + " = ");
+				
+				switch(whichCalculation) {
+				case 1: Addition(); break;
+				case 2: Subtraction(); break;
+				case 3: Multiplication(); break;
+				case 4: Division(); break;
+				}
+				
 				answer = input.nextInt();
 				if (answer == result) {
 					System.out.println("Correct!");
@@ -57,17 +88,19 @@ public class AdditionGame_04 {
 					System.out.println("It's wrong.\nThe correct answer is " + result + "!");
 				}
 				System.out.println("");
+				
 				stopTime = System.currentTimeMillis();
 				answerTime = (stopTime - startTime) / 1000;
 				totalTime += answerTime;
 			}
-			
+
 			scorePercentage = (correctCounter/(double)howManyGames) * 100;
 			System.out.println(userName + "'s score is " + correctCounter + " out of " + howManyGames + ".");
 			System.out.println(userName + "'s score is " + String.format("%.2f", scorePercentage) + "%.");
 			System.out.println(userName + " took " + String.format("%.2f", totalTime) + " seconds.");
 			System.out.println(userName + "'s average seconds per answer is " + String.format("%.2f", totalTime/howManyGames) + " seconds.");
 
+			// RESET
 			correctCounter = 0;
 			totalTime = 0;
 			
@@ -75,16 +108,67 @@ public class AdditionGame_04 {
 			System.out.println("Would you like to try again?");
 			System.out.print("Press any key to try again OR Press '0' to stop: ");
 			keepGoingCheck = input.next().charAt(0);
-			System.out.println("");
 			if(keepGoingCheck == '0') {
-				System.out.println("\nThank you and good bye.");
+				System.out.println("\n\nThank you and good bye.");
 				keepGoing = false;
 				break;
 			}
 		}
 
-		input.close();
 
+
+
+		input.close();
 	} // End of Main Method
 
+
+
+
+
+	private static void Addition() {
+		result = ranNum1 + ranNum2;
+		System.out.print(ranNum1 + " + " + ranNum2 + " = ");
+	}
+
+
+
+
+
+	private static void Subtraction() {
+		int max;
+		if (ranNum2 > ranNum1) {
+			max = ranNum2;
+			ranNum2 = ranNum1;
+			ranNum1 = max;
+		}
+		result = ranNum1 - ranNum2;
+		System.out.print(ranNum1 + " - " + ranNum2 + " = ");
+	}
+
+
+
+
+
+	private static void Multiplication() {
+		result = ranNum1 * ranNum2;
+		System.out.print(ranNum1 + " X " + ranNum2 + " = ");	
+	}
+
+
+
+
+
+	private static void Division() {
+		int max;
+		if (ranNum2 > ranNum1) {
+			max = ranNum2;
+			ranNum2 = ranNum1;
+			ranNum1 = max;
+		}
+		result = ranNum1 / ranNum2;
+		System.out.print(ranNum1 + " / " + ranNum2 + " = ");
+	}
+	
+	
+	
 }
